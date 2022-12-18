e8649a36d101926c670e03c88dcd2ce47af6aa8c
SebastianEspitiaS <espitiasanchezsebastian64@gmail.com>
2022-12-18 12:24:53 -0500

    registro de productos

--- Examen1,2.py
+++ Examen1,2.py
@@ -5,53 +5,91 @@ def borrarPantalla():
     elif os.name == "ce" or os.name == "nt" or os.name == "dos":
         os.system ("cls")
 
-productos = []
+def ingresarProductos(producto):
+    print('Ingrese un nuevo producto.')
+    datosP[0] = input('Ingrese el nombre del producto: ').capitalize()
+    datosP[1] = int(input('Ingrese el valor del producto: '))
+    datosP[2] = input('Ingrese el código del producto: ')
+    producto[datosP[0]] = [datosP[1], datosP[2]]
+    return producto
+
+def imprimirProductos(producto):
+    print('Los productos ingresados son:\n')
+    for i, d in producto.items():
+        print(f'{i:10} -> vale: {d[0]:5} código: {d[1]}')
+    print('')
+
+def actualizarProductos(producto):
+    pass
+
+def eliminarProductos(producto):
+    while True:
+            datosP[0] = input(('Quieres eliminar un elemento de la lista? (S/N): '))
+            if not datosP[0].lower() in ['si', 's', 'no', 'n']:
+                print('Digita una opción correcta...')
+                continue
+            elif datosP[0].lower() in ['si', 's']:
+                datosP[0] = input('Digita el nombre del producto que quieres eliminar: ').capitalize()
+                if producto.get(datosP[0]) == None:
+                    print('El nombre que acaba de ingresar no esta en la lista de productos...')
+                else:
+                    del producto[datosP[0]]
+                    print('El producto se ha eliminado con éxito...')
+            break
+    return producto
+
 datosP = [None,None,None]
 producto = {}
+option = None
 
-while True:
+while option != '0':
+    print('1. Ingresar un nuevo producto \n2. Mostrar los productos registrados \n3. Actualizar producto \n4. Eliminar un producto \n0. Salir')
+    option = input('Digita una opción: ')
 
-    print('1. recibir nombre de un producto \n2. mostrar los productos registrados \n3. agregar un nuevo producto \n4. eliminar un producto \n0. Salir')
+    if option == '1':
+        ingresarProductos(producto)
 
-    opcion = int(input("Digita una opción: "))
+    elif option == '2':
+        imprimirProductos(producto)
 
-    match (opcion):
-        case (1):
-            print("ingrese un nuevo producto:")
-            datosP[0] = input('ingrese el nombre del producto: ')
-            datosP[1] = int(input('ingrese el valor del producto: '))
-            datosP[2] = input('ingrese el código del producto: ')
+    elif option == '3':
+        pass
 
-            producto[datosP[0]] = [datosP[1], datosP[2]]
+    elif option == '4':
+        eliminarProductos(producto)
 
-            """producto["codigo"] = input('ingrese el código del producto: ')
-            producto["nombre"] = input('ingrese el nombre del producto: ')
-            producto["precio"] = int(input('ingrese el valor del producto: '))
-            productos.append(producto)"""
+    else:
+        print('Ingresa una opción correcta...')
+    entrada = input('Presione la tecla "ENTER" para volver al menu')
+    borrarPantalla()
 
+borrarPantalla()
+print('Fin del programa...\n')
 
-            entrada = input('presione la tecla "ENTER" para volver al menu')
-            borrarPantalla()
+"""
+productos = []
+producto["código"] = input('ingrese el código del producto: ')
+producto["nombre"] = input('ingrese el nombre del producto: ')
+producto["precio"] = int(input('ingrese el valor del producto: '))
+productos.append(producto)
+"""
 
-        case (2):
-            print('los los productos ingresados son: ')
-            for i, d in producto.items():
-                print(f'{i} vale : {d[0]} codigo : {d[1]}')
-            entrada = input('presione la tecla "ENTER" para volver al menu')
-            borrarPantalla()
-
-
-    ''''case(3):
-            datosP[0] = input('Digita el nombre del producto que quieres actualizar: ')
-            if productos.get(datosP[0]) != None:
-                try:
-                    datosP[1] = int(input('ingrese un nuevo valor para el producto: '))
-                except:
-                    print('el dato ingresado no es valido: ')
-                else:
-
-                    datosP[2] = input(('quiere cambiar el código del producto? si/no (s/n)'))
-                    if datosP[2].lower() == 'si' or 's':
-                        datosP[2] =
-            case (_):
-            pass'''
+'''datosP[0] = input('Digita el nombre del producto que quieres actualizar: ').capitalize()
+        if producto.get(datosP[0]) == None:
+            print('El nombre que acaba de ingresar no esta en la lista de productos...')
+        else:
+            try:
+                datosP[1] = int(input('Ingrese un nuevo valor para el producto: '))
+            except:
+                print('El dato ingresado no es valido... ')
+            else:
+                producto [datosP[0]] [0] = datosP[1]
+                while True:
+                    datosP[2] = input(('Quiere cambiar el código del producto? (S/N): '))
+                    if not datosP[2].lower() in ['si', 's', 'no', 'n']:
+                        print('Digite una opción correcta...')
+                        continue
+                    elif datosP[2].lower() in ['si', 's']:
+                        datosP[2] = input("Ingrese el nuevo código del producto: ")
+                        producto [datosP[0]] [1] = datosP[2]
+                    break'''
